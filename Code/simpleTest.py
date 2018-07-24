a6bf59dbd7214daa8672a37b1c1e170713ddf46c
Iván Rodríguez <ivan.traceur@gmail.com>
2018-07-24 11:35:33 +0100

    Añadimos la licencia al código

--- Code/simpleTest.py
+++ Code/simpleTest.py
@@ -1,3 +1,21 @@
+'''
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Ivan Rodriguez - 2018
+Original Code -> https://www.nathankowald.com/blog/2017/05/dash-button-with-raspberry-pi/
+'''
+
 from pydhcplib.dhcp_network import *
 
 def do_something():
